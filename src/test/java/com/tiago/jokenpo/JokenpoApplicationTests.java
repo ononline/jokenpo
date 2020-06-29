6cad9ce75c931ff9e69a2411f15390504d8aa83f
ononline <tiago.rag@gmail.com>
2020-06-29 00:37:48 -0300

    Add files via upload

---
+++ src/test/java/com/tiago/jokenpo/JokenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.jokenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
